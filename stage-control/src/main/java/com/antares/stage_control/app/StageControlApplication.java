284c9268994eb8dcfcb2c70a02b7c9627beac6ca
Rudy Ruiz <rruizjulca@MacBook-Pro-de-Rudy.local>
2024-10-20 18:37:56 +0200

    primera carga

---
+++ stage-control/src/main/java/com/antares/stage_control/app/StageControlApplication.java
@@ -0,0 +1,13 @@
+package com.antares.stage_control.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StageControlApplication.class, args);
+	}
+
+}
---
+++ stage-control/src/test/java/com/antares/stage_control/app/StageControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antares.stage_control.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
